7c7f4a6efb8c580286048cc98ca73f3ef0853347
Codenator <codenator81@gmail.com>
2016-05-04 00:04:19 +0300

    Change little folders

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -3,6 +3,7 @@ import {Component} from '@angular/core';
 @Component({
     selector: 'ng2-app',
     template: `<h1>Hello Angular</h1>
+    <p>new tag</p>
     `,
 
 })
--- gulpfile.js
+++ gulpfile.js
@@ -6,31 +6,25 @@ var assetsProd = 'src/';
 var appDev = 'dev/';
 var appProd = 'app/';
 
-/* Mixed */
-var ext_replace = require('gulp-ext-replace');
-
 /* CSS */
-var postcss = require('gulp-postcss');
 var sourcemaps = require('gulp-sourcemaps');
-var autoprefixer = require('autoprefixer');
-var precss = require('precss');
-var cssnano = require('cssnano');
+var autoprefixer = require('gulp-autoprefixer');
+var sass = require('gulp-sass');
 
 /* JS & TS */
-var jsuglify = require('gulp-uglify');
 var typescript = require('gulp-typescript');
-
-/* Images */
-var imagemin = require('gulp-imagemin');
 
 var tsProject = typescript.createProject('tsconfig.json');
 
 gulp.task('build-css', function () {
     return gulp.src(assetsDev + 'scss/*.scss')
         .pipe(sourcemaps.init())
-        .pipe(postcss([precss, autoprefixer, cssnano]))
+        .pipe(sass({}).on('error', sass.logError))
         .pipe(sourcemaps.write())
-        .pipe(ext_replace('.css'))
+        .pipe(autoprefixer({
+            browsers: ['last 3 versions'],
+            cascade: false
+        }))
         .pipe(gulp.dest(assetsProd + 'css/'));
 });
 
@@ -39,27 +33,30 @@ gulp.task('build-ts', function () {
         .pipe(sourcemaps.init())
         .pipe(typescript(tsProject))
         .pipe(sourcemaps.write())
-        //.pipe(jsuglify())
         .pipe(gulp.dest(appProd));
 });
 
-gulp.task('build-img', function () {
-    return gulp.src(assetsDev + 'img/**/*')
-        .pipe(imagemin({
-            progressive: true
-        }))
-        .pipe(gulp.dest(assetsProd + 'img/'));
+gulp.task('bundle-ts', ['build-ts'], function() {
+    var path = require("path");
+    var Builder = require('systemjs-builder');
+    
+    var builder = new Builder('', 'systemjs.config.js');
+
+    builder
+        .buildStatic('app/main.js', 'app/bundle.js', { minify: true, sourceMaps: true})
+        .then(function() {
+            console.log('Build complete');
+        })
+        .catch(function(err) {
+            console.log('Build error');
+            console.log(err);
+        });
 });
 
-gulp.task('build-html', function () {
-    return gulp.src(appDev + '**/*.html')
-        .pipe(gulp.dest(appProd));
-});
 
 gulp.task('watch', function () {
-    gulp.watch(appDev + '**/*.ts', ['build-ts']);
+    gulp.watch(appDev + '**/*.ts', ['build-ts', 'bundle-ts']);
     gulp.watch(assetsDev + 'scss/**/*.scss', ['build-css']);
-    gulp.watch(assetsDev + 'img/*', ['build-img']);
 });
 
-gulp.task('default', ['watch', 'build-ts', 'build-css']);
+gulp.task('default', ['watch', 'build-ts', 'bundle-ts', 'build-css']);
